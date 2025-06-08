e24254f59a5f5ef66771f7ec2a4e3f6b031f5f8f
Thomas Papathymiopoulos <59363938+Thomatous@users.noreply.github.com>
2025-06-08 12:12:37 +0300

    Add resolve_basemap_name common method

--- chartops/common.py
+++ chartops/common.py
@@ -1,6 +1,13 @@
-"""The common module contains common functions and classes used by the other modules."""
+from typing import Any
 
+def resolve_basemap_name(basemap_name: str) -> Any:
+    from ipyleaflet import basemaps
 
-def hello_world():
-    """Prints "Hello World!" to the console."""
-    print("Hello World!")
+    basemaps_obj = basemaps
+    for basemap_name_part in basemap_name.split('.'):
+        if hasattr(basemaps_obj, basemap_name_part):
+            basemaps_obj = getattr(basemaps_obj, basemap_name_part)
+        else:
+            raise AttributeError(f"Unsupported basemap: {basemap_name}")
+
+    return basemaps_obj
---
+++ tests/test_common.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import unittest
+
+from chartops import common
+
+class TestCommon(unittest.TestCase):
+    def test_resolve_simple_basemap_name(self) -> None:
+        from ipyleaflet import basemaps
+        basemap_name = "OpenStreetMap"
+        basemap = common.resolve_basemap_name(basemap_name)
+        expected_basemap = getattr(basemaps, basemap_name)
+        self.assertIs(basemap, expected_basemap)
+
+    def test_resolve_nested_basemap_name(self) -> None:
+        from ipyleaflet import basemaps
+        basemap_name = "Esri.WorldImagery"
+        basemap = common.resolve_basemap_name(basemap_name)
+        expected_basemap = getattr(getattr(basemaps, "Esri"), "WorldImagery")
+        self.assertIs(basemap, expected_basemap)
+    
+    def test_resolve_invalid_basemap_name(self) -> None:
+        basemap_name = "InvalidBasemap"
+        with self.assertRaises(AttributeError):
+            common.resolve_basemap_name(basemap_name)
+
+
