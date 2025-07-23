f2514c1fb89d47b3a8cbf230082e956761d2bd61
Thomas Papathymiopoulos <59363938+Thomatous@users.noreply.github.com>
2025-07-23 12:25:20 +0300

    Enhance testing

--- tests/test_chartops.py
+++ tests/test_chartops.py
@@ -11,7 +11,7 @@ import pandas as pd
 import geopandas as gpd
 from pathlib import Path
 from chartops import chartops
-from ipyleaflet import LayersControl, GeoJSON, ImageOverlay, WMSLayer, VideoOverlay
+from ipyleaflet import LayersControl, GeoJSON, ImageOverlay, WMSLayer, VideoOverlay, WidgetControl
 from unittest.mock import patch
 
 
@@ -59,11 +59,12 @@ class TestChartops(unittest.TestCase):
     def test_adding_layer_control_with_all_positions(self) -> None:
         positions = ["topright", "topleft", "bottomright", "bottomleft"]
         for position in positions:
-            map_instance = chartops.Map()
-            map_instance.add_layer_control(position)
-            control = map_instance.controls[-1]
-            self.assertIsInstance(control, LayersControl)
-            self.assertEqual(getattr(control, "position"), position)
+            with self.subTest(position=position):
+                map_instance = chartops.Map()
+                map_instance.add_layer_control(position)
+                control = map_instance.controls[-1]
+                self.assertIsInstance(control, LayersControl)
+                self.assertEqual(getattr(control, "position"), position)
 
     def test_adding_a_vector_layer_from_a_url(self) -> None:
         self.map.add_vector(
@@ -382,5 +383,19 @@ class TestChartops(unittest.TestCase):
                 )
             self.assertIn("Failed to add WMS layer", str(cm.exception))
 
-    def test_add_basemap_gui(self):
-        self.map.add_basemap_gui()
+    def test_get_basemap_layers_and_latest(self):
+        basemap_layers = self.map._get_basemap_layers()
+        self.assertEqual(len(basemap_layers), 1)
+        self.assertEqual(basemap_layers[0].name, "OpenStreetMap.Mapnik")
+
+        with self.subTest("Adding a single basemap"):
+            self.map.add_basemap("Esri.WorldImagery")
+            basemap_layers = self.map._get_basemap_layers()
+            self.assertEqual(len(basemap_layers), 2)
+            self.assertEqual(self.map._get_latest_basemap_layer().name, "Esri.WorldImagery")
+
+        with self.subTest("Adding multiple basemaps"):
+            self.map.add_basemap("OpenTopoMap")
+            basemap_layers = self.map._get_basemap_layers()
+            self.assertEqual(len(basemap_layers), 3)
+            self.assertEqual(self.map._get_latest_basemap_layer().name, "OpenTopoMap")
